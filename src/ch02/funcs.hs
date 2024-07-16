1ff1909c2fced39cc7d041cca2eb1ff94eebb271
Graham Strickland <grahamhstrickland@gmail.com>
2024-07-16 19:04:17 +0200

    Add functions from Ex. 2.5. to funcs.hs

--- src/ch02/funcs.hs
+++ src/ch02/funcs.hs
@@ -3,3 +3,21 @@ f x = x**(1/3)
 
 g :: Double -> Double
 g y = exp y + 8**y
+
+h :: Double -> Double
+h x = 1 / sqrt ((x - 5)^2 + 16)
+
+gamma :: Double -> Double
+gamma beta = 1 / sqrt (1 - beta^2)
+
+bigU :: Double -> Double
+bigU x = (1 / (10 + x)) + (1 / (10 - x))
+
+bigL :: Double -> Double
+bigL l = sqrt $ l * (l + 1)
+
+bigE :: Double -> Double
+bigE x = 1 / abs x ^ 3
+
+bigE2 :: Double -> Double
+bigE2 z = 1 / (z^2 + 4)**(3/2)
