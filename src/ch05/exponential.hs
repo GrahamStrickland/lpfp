bbde149c410451340856788daf87d30555d7a3db
Graham Strickland <grahamhstrickland@gmail.com>
2025-03-11 21:34:31 +0200

    Add first attempt at calculation of min approximation counter for expList and add code references to ch05

--- src/ch05/exponential.hs
+++ src/ch05/exponential.hs
@@ -1,7 +1,19 @@
 type R = Double
 
 expList :: R -> [R]
-expList x = [(1.0 + x / n) ** n | n <- [1..]]
+expList x = [(1.0 + x / n) ** n | n <- [1 ..]]
 
-calcMinErr :: R -> R -> Int
-calcMinErr x eps = abs (exp x - expList x)
+expErr :: R -> R -> R
+expErr x approx = abs (exp x - approx)
+
+listExpErr :: [R] -> R -> [R]
+listExpErr es x = [expErr x e | e <- es]
+
+withinTolerance :: [R] -> R -> Bool
+withinTolerance xs eps = last xs < eps
+
+calcMinExpErr :: Int -> R -> R -> Int
+calcMinExpErr n x eps =
+  if withinTolerance (take n (listExpErr (expList x) x)) eps
+    then n
+    else calcMinExpErr (n + 1) x eps
