27e507c7762f40913b5d6d0c9765dd646a6efd9e
Graham Strickland <grahamhstrickland@gmail.com>
2025-07-17 20:41:43 +0200

    Add Ch10.SimpleVec module and some associated functions

---
+++ src/Ch10/SimpleVec.hs
@@ -0,0 +1,107 @@
+{-# OPTIONS -Wall #-}
+
+module Ch10.SimpleVec where
+
+infixl 6 ^+^
+infixl 6 ^-^
+infixr 7 *^
+infixl 7 ^*
+infixr 7 ^/
+infixr 7 <.>
+infixl 7 ><
+
+type R = Double
+
+data Vec = Vec { xComp :: R -- x component
+               , yComp :: R -- y component
+               , zComp :: R -- z component
+               } deriving (Eq)
+
+instance Show Vec where
+    show (Vec x y z) = "vec " ++ showDouble x ++ " "
+                              ++ showDouble y ++ " "
+                              ++ showDouble z
+
+showDouble :: R -> String
+showDouble x
+    | x < 0     = "(" ++ show x ++ ")"
+    | otherwise = show x
+
+
+-- Form a vector by giving its x, y, and z components.
+vec :: R -- x component
+    -> R -- y component
+    -> R -- z component
+    -> Vec
+vec = Vec
+
+iHat :: Vec
+iHat = vec 1 0 0 
+
+jHat :: Vec
+jHat = vec 0 1 0
+
+kHat :: Vec
+kHat = vec 0 0 1
+
+zeroV :: Vec
+zeroV = vec 0 0 0
+
+negateV :: Vec -> Vec
+negateV (Vec ax ay az) = Vec (-ax) (-ay) (-az)
+
+(^+^) :: Vec -> Vec -> Vec
+Vec ax ay az ^+^ Vec bx by bz = Vec (ax+bx) (ay+by) (az+bz)
+
+(^-^) :: Vec -> Vec -> Vec
+Vec ax ay az ^-^ Vec bx by bz = Vec (ax-bx) (ay-by) (az-bz)
+
+sumV :: [Vec] -> Vec
+sumV = foldr (^+^) zeroV
+
+(*^)  :: R   -> Vec -> Vec
+c *^ Vec ax ay az = Vec (c*ax) (c*ay) (c*az)
+
+(^*)  :: Vec -> R   -> Vec
+Vec ax ay az ^* c = Vec (c*ax) (c*ay) (c*az)
+
+(<.>) :: Vec -> Vec -> R
+Vec ax ay az <.> Vec bx by bz = ax*bx + ay*by + az*bz
+
+(><)  :: Vec -> Vec -> Vec
+Vec ax ay az >< Vec bx by bz
+    = Vec (ay*bz - az*by) (az*bx - ax*bz) (ax*by - ay*bx)
+
+(^/) :: Vec -> R -> Vec
+Vec ax ay az ^/ c = Vec (ax/c) (ay/c) (az/c)
+
+magnitude :: Vec -> R
+magnitude v = sqrt(v <.> v)
+
+type VecDerivative = (R -> Vec) -> R -> Vec
+
+vecDerivative :: R -> VecDerivative
+vecDerivative dt v t = (v (t + dt/2) ^-^ v (t - dt/2)) ^/ dt
+
+v1 :: R -> Vec
+v1 t = 2 *^ t**2 *^ iHat ^+^ 3 *^ t**3 *^ jHat ^+^ t**4 *^ kHat
+
+xCompFunc :: (R -> Vec) -> R -> R
+xCompFunc v t = xComp (v t)
+
+yCompFunc :: (R -> Vec) -> R -> R
+yCompFunc v t = yComp (v t)
+
+zCompFunc :: (R -> Vec) -> R -> R
+zCompFunc v t = zComp (v t)
+
+type Derivative = (R -> R) -> R -> R
+
+derivative :: R -> Derivative
+derivative dt x t = (x (t + dt/2) - x (t - dt/2)) / dt
+
+type Time         = R
+type PosVec       = Vec
+type Velocity     = Vec
+type Acceleration = Vec
+
