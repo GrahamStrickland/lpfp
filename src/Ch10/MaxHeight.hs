7e6d9e5df4d6f00b6159a9b86a820b4aa2d10ec2
Graham Strickland <grahamhstrickland@gmail.com>
2025-07-20 21:36:12 +0200

    Add first attempt at exercise 10.3

--- src/Ch10/MaxHeight.hs
+++ src/Ch10/MaxHeight.hs
@@ -1,4 +1,13 @@
 module Ch10.MaxHeight where
 
+import Ch10.SimpleVec
+import Ch10.Kinematics3D
+
+zCompProjectilePos :: PosVec -> Velocity -> Time -> R
+zCompProjectilePos r0 v0 t = zComp (projectilePos r0 v0 t)
+
+positiveHeightTimes :: PosVec -> Velocity -> [R]
+positiveHeightTimes r0 v0 = takeWhile (\t -> zCompProjectilePos r0 v0 t > 0) [0,0.1..]
+
 maxHeight :: PosVec -> Velocity -> R
-maxHeight = undefined
+maxHeight r0 v0 = maximum [zCompProjectilePos r0 v0 t | t <- positiveHeightTimes r0 v0]
