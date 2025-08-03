00c48be71239e6fb93f5555eee78c52d7dc13290
Graham Strickland <grahamhstrickland@gmail.com>
2025-08-03 16:43:35 +0200

    Fix spacing of MaxHeight.hs

--- src/Ch10/MaxHeight.hs
+++ src/Ch10/MaxHeight.hs
@@ -7,7 +7,9 @@ zCompProjectilePos :: PosVec -> Velocity -> Time -> R
 zCompProjectilePos r0 v0 t = zComp (projectilePos r0 v0 t)
 
 positiveHeightTimes :: PosVec -> Velocity -> [R]
-positiveHeightTimes r0 v0 = takeWhile (\t -> zCompProjectilePos r0 v0 t > 0) [0,0.1..]
+positiveHeightTimes r0 v0 
+    = takeWhile (\t -> zCompProjectilePos r0 v0 t > 0) [0,0.1..]
 
 maxHeight :: PosVec -> Velocity -> R
-maxHeight r0 v0 = maximum [zCompProjectilePos r0 v0 t | t <- positiveHeightTimes r0 v0]
+maxHeight r0 v0 
+    = maximum [zCompProjectilePos r0 v0 t | t <- positiveHeightTimes r0 v0]
