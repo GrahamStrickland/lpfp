7dce2cfbea754b3df274c8fbf6b3d8940212a543
Graham Strickland <grahamhstrickland@gmail.com>
2025-09-21 21:41:07 +0200

    Add first attempt at exercise 10.9

---
+++ src/Ch10/BallSpeed.hs
@@ -0,0 +1,25 @@
+module Ch10.BallSpeed where
+
+import Graphics.Gnuplot.Simple
+
+import Ch10.Kinematics3D
+import Ch10.SimpleVec
+
+gEarth :: Vec
+gEarth = negateV (9.8 *^ kHat)
+
+vecFromMagAngle :: (R, R) -> Vec
+vecFromMagAngle (r, theta) = Vec 0 (r * cos theta) (r * sin theta)
+
+vBall :: R -> Vec
+vBall t = ((vecFromMagAngle (25, 52)) ^* t) ^+^ (gEarth ^* (0.5 * t ** 2))
+
+speedRateChangeBall :: R -> R
+speedRateChangeBall t = speedRateChange (vBall t) gEarth
+
+speedRateChangeBallPlot :: IO ()
+speedRateChangeBallPlot =
+    plotFunc
+        [Key (Just ["noautotitle"])]
+        [0, 0.1 .. 4]
+        speedRateChangeBall
