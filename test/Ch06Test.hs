680e9ffe12a63182d28b5875325af31615266b67
Graham Strickland <grahamhstrickland@gmail.com>
2025-11-22 11:09:21 +0200

    Add tests and cut LaTeX for remaining exercises in chapter 6

--- test/Ch06Test.hs
+++ test/Ch06Test.hs
@@ -1,7 +1,12 @@
 module Ch06Test where
 
+import qualified Ch06.Average as Average
+import qualified Ch06.Filter as Filter
 import qualified Ch06.Geq as Geq
+import qualified Ch06.Map as Map
 import qualified Ch06.Predicate as Predicate
+import qualified Ch06.Repeat as Repeat
+import qualified Ch06.Replicate as Replicate
 import qualified Ch06.Squares as Squares
 import Test.HUnit
 
@@ -12,6 +17,11 @@ ch06Tests =
             [ testCh06Geq
             , testCh06Predicate
             , testCh06Squares
+            , testCh06Repeat
+            , testCh06Replicate
+            , testCh06Map
+            , testCh06Filter
+            , testCh06Average
             ]
 
 testCh06Geq :: Test
@@ -41,3 +51,33 @@ testCh06Squares =
     TestLabel "Ch06.Squares tests" $
         TestList
             [TestCase $ assertEqual "Test take 10 Squares.first1000Squares" [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] (take 10 Squares.first1000Squares)]
+
+testCh06Repeat :: Test
+testCh06Repeat =
+    TestLabel "Ch06.Repeat tests" $
+        TestList
+            [TestCase $ assertEqual "Test take 10 Repeat.repeat'" "xxxxxxxxxx" (take 10 (Repeat.repeat' 'x'))]
+
+testCh06Replicate :: Test
+testCh06Replicate =
+    TestLabel "Ch06.Replicate tests" $
+        TestList
+            [TestCase $ assertEqual "Test Replicate.replicate'" "xxx" (Replicate.replicate' 3 'x')]
+
+testCh06Map :: Test
+testCh06Map =
+    TestLabel "Ch06.Map tests" $
+        TestList
+            [TestCase $ assertEqual "Test Map.map' sqrt" [1.0, 2.0, 3.0] (Map.map' sqrt [1, 4, 9])]
+
+testCh06Filter :: Test
+testCh06Filter =
+    TestLabel "Ch06.Filter tests" $
+        TestList
+            [TestCase $ assertEqual "Test Filter.filter' (\n -> n < 10)" [6, 4, 8, 7] (Filter.filter' (\n -> n < 10) [6, 4, 8, 13, 7])]
+
+testCh06Average :: Test
+testCh06Average =
+    TestLabel "Ch06.Average tests" $
+        TestList
+            [TestCase $ assertEqual "Test Average.average" 2.0 (Average.average [1.0, 2.0, 3.0])]
