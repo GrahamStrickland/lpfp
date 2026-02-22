19111fd55713a4e2d46ad62a3963a9f5dd08dbf8
Graham Strickland <grahamhstrickland@gmail.com>
2026-02-22 20:34:57 +0200

    Cut redundant brackets from UniformCircularMotion.hs

--- src/Ch10/UniformCircularMotion.hs
+++ src/Ch10/UniformCircularMotion.hs
@@ -1,13 +1,12 @@
 module Ch10.UniformCircularMotion where
 
-import Ch10.Kinematics3D
 import Ch10.SimpleVec
 
 rUCM :: R -> R -> R -> Vec
-rUCM omega r t = r *^ (vec (cos (omega * t)) (sin (omega * t)) 0)
+rUCM omega r t = r *^ vec (cos (omega * t)) (sin (omega * t)) 0
 
 vUCM :: R -> R -> R -> Vec
-vUCM omega r t = (omega * r) *^ (vec (-(sin (omega * t))) (cos (omega * t)) 0)
+vUCM omega r t = (omega * r) *^ vec (-(sin (omega * t))) (cos (omega * t)) 0
 
 aUCM :: R -> R -> R -> Vec
-aUCM omega r t = ((-(omega ** 2)) * r) *^ (vec (cos (omega * t)) (sin (omega * t)) 0)
+aUCM omega r t = ((-(omega ** 2)) * r) *^ vec (cos (omega * t)) (sin (omega * t)) 0
